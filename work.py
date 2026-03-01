7342f57107d0cb8f51ca14445f1251f9861fbef2
AtulGadkoti <atulgadkoti@gmail.com>
2026-03-01 21:48:36 +0530

    Initial commit: scheduled API ping workflow

---
+++ work.py
@@ -0,0 +1,11 @@
+import requests
+import os
+API_URL = os.environ["API_URL"]
+
+def make_request():
+    response = requests.get(API_URL, timeout=500)
+    print("Status:", response.status_code)
+    print("Response:", response.text[:200])
+
+if __name__ == "__main__":
+    make_request()
